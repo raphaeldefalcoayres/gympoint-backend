3d7d4975a452c8ff732fb45c97b204425e9a080f
Raphael de Falco Ayres <raphaeldefalcoayres@gmail.com>
2019-11-26 20:24:32 -0300

    CRUDE plans ok

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import authMidleware from './app/middlewares/auth';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import StudentController from './app/controllers/StudentController';
+import PlanController from './app/controllers/PlanController';
 
 const routes = new Router();
 
@@ -17,5 +18,10 @@ routes.put('/users', UserController.update);
 
 routes.post('/students', StudentController.store);
 routes.put('/students', StudentController.update);
+
+routes.get('/plans', PlanController.index);
+routes.post('/plans', PlanController.store);
+routes.put('/plans/:id', PlanController.update);
+routes.delete('/plans/:id', PlanController.delete);
 
 export default routes;
