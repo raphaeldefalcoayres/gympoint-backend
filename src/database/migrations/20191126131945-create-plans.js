9bf8359b6b57d23ed3bc149f176e0e3e89993aea
Raphael de Falco Ayres <raphaeldefalcoayres@gmail.com>
2019-11-26 10:40:58 -0300

    create migration and seed for plans

---
+++ src/database/migrations/20191126131945-create-plans.js
@@ -0,0 +1,32 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('plans', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        type: Sequelize.STRING,
+      },
+      duration: {
+        type: Sequelize.INTEGER,
+      },
+      price: {
+        type: Sequelize.FLOAT,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: queryInterface => {
+    return queryInterface.dropTable('plans');
+  },
+};
